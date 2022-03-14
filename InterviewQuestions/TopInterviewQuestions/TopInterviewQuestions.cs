968b76f064771d173320d195995efbe0be09996b
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-03-14 01:01:48 -0400

    Update TopInterviewQuestions.cs

--- InterviewQuestions/TopInterviewQuestions/TopInterviewQuestions.cs
+++ InterviewQuestions/TopInterviewQuestions/TopInterviewQuestions.cs
@@ -1,26 +1,26 @@
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 
+using static System.Console;
 namespace InterviewQuestions.TopInterviewQuestions;
 
 /// <summary>
 /// A set of solutions based on the following questions found on the link
 /// https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/
 /// </summary>
-public class TopInterviewQuestions
+internal class TopInterviewQuestions
 {
-    public void Initialize()
+    public static void Initialize()
     {
         // Rotate a given array k number of steps
         // https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/646/
         var rotateArray = new RotateArrayQuestion();
         var array = new int[] {1, 2, 4, 5, 6, 7};
-        var numberOfStesp = 4;
-        rotateArray.Rotate(array, numberOfStesp);
+        const int numberOfSteps = 4;
+        WriteLine("###### Rotate a give array {1, 2, 4, 5, 6, 7} with the following number of steps "  + $"{numberOfSteps}");
+        rotateArray.Rotate(array, numberOfSteps);
         foreach (var items in array)
         {
-            Console.Write(items + ",");
+            Write(items + ",");
         }
-        Console.WriteLine();
+        WriteLine();
     }
 }
